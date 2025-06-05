7a45ba9c7c225a46d4c304e40a4c082eb7d77bf2
Dycouzt <dieacost@outlook.com>
2025-06-05 22:45:11 +0200

    lil update

--- WebScraping/ScrapingStuff.py
+++ WebScraping/ScrapingStuff.py
@@ -1,1 +1,12 @@
-# This script shows how web scraping works using the beautifulsoup module in python.
+# This script shows how web scraping works using the beautifulsoup module in python
+import requests
+from bs4 import BeautifulSoup
+
+
+url = "https://quotes.toscrape.com"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+authors = soup.find_all('span', class_='author')
+for author in authors:
+    print(author.text)
