a339d3b02e10d6bc31e996ebe0f7c35818aea238
Dycouzt <dieacost@outlook.com>
2025-06-07 23:49:10 +0200

    Update

--- WebScraping/ScrapingStuff.py
+++ WebScraping/ScrapingStuff.py
@@ -7,6 +7,18 @@ url = "https://quotes.toscrape.com"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-authors = soup.find_all('span', class_='author')
-for author in authors:
-    print(author.text)
+# Easy Exercises
+
+first_quote = soup.find('span', class_='text')
+print(first_quote.text)
+
+quotes = soup.find_all('small', class_='author')
+for quote in quotes:
+    print(quote.text)
+
+tags = soup.find_all('div', class_='tags')
+for tag in tags[:5]:
+    print(tag.text)
+
+# Intermediate Exercises
+
