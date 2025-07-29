8a810b09e1b34a9952cd897a4af5292bc96ab3cf
Dycouzt <dieacost@outlook.com>
2025-07-28 23:43:42 -0600

    automation 101

--- SocketProgramming/TCPClient.py
+++ SocketProgramming/TCPClient.py
@@ -5,19 +5,12 @@ message = input("Message: ")
 try:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_client:
         tcp_client.connect(('localhost', 9999))
-        tcp_client.send(message.encode())
 
+        tcp_client.send(message.encode())
         print(f"message sent!")
 
-        conn, addr = tcp_client.accept()
-        print(f"Connected by {addr}")
-
-        data = conn.recv(1024).decode()
-        print(f"{data} received from {addr}! ")
-
-        conn.close()
-        tcp_client.close()
-        print("Server Closed! ")
+        data = tcp_client.recv(1024)
+        print(f"Received from server: {data}")
 
 except ConnectionRefusedError:
     print("Connection refused!")
