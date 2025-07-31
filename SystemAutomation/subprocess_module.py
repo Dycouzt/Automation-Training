973410bec8654de21bbf8b5accf56bb9a556e4d8
Dycouzt <dieacost@outlook.com>
2025-07-30 23:50:00 -0600

    subprocess module updated

--- SystemAutomation/subprocess_module.py
+++ SystemAutomation/subprocess_module.py
@@ -5,3 +5,7 @@ result = subprocess.run(['ls', '-la'], capture_output=True, text=True)
 
 # Output the result
 print(result.stdout)
+
+ping_result = subprocess.run(['ping', 'google.com'], capture_output=True, text=True)
+
+print(ping_result.stdout)
