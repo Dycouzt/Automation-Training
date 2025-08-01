2e1758fe23ce4d1cb8d8427ebdac136b39c4f79f
Dycouzt <dieacost@outlook.com>
2025-07-31 23:57:24 -0600

    os module updated

--- SystemAutomation/os_module.py
+++ SystemAutomation/os_module.py
@@ -1,5 +1,23 @@
 import os
 
+
+''' Example usage: 
 os.mkdir('new_folder')
 os.rename('old_name.txt', 'new_name.txt')
 print(os.getenv('HOME'))
+'''
+
+# Create a directory
+os.mkdir("new_folder")
+
+# List contents of a directory
+print(os.listdir("."))  # Lists files in current directory
+
+# Rename a file or folder
+os.rename("new_folder", "renamed_folder")
+
+# Remove a file
+os.remove("example.txt")
+
+# Remove a directory (must be empty)
+os.rmdir("renamed_folder")
