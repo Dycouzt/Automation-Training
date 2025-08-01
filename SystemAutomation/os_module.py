b136241e4f8362f12ff72b6efd587a4e490ff32c
Dycouzt <dieacost@outlook.com>
2025-07-31 23:57:48 -0600

    os module updated

--- SystemAutomation/os_module.py
+++ SystemAutomation/os_module.py
@@ -21,3 +21,19 @@ os.remove("example.txt")
 
 # Remove a directory (must be empty)
 os.rmdir("renamed_folder")
+
+# Join paths safely
+full_path = os.path.join("/home/user", "file.txt")
+
+# Check existence
+print(os.path.exists(full_path))
+
+# Check if it's a file or directory
+print(os.path.isfile(full_path))
+print(os.path.isdir(full_path))
+
+# Get file size
+print(os.path.getsize(full_path))
+
+# Get absolute path
+print(os.path.abspath("file.txt"))
