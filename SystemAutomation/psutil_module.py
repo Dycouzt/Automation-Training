e9e83ea26ba3f0cc682282408337292743cdc185
Dycouzt <dieacost@outlook.com>
2025-08-08 22:51:32 -0600

    psutil module updated

--- SystemAutomation/psutil_module.py
+++ SystemAutomation/psutil_module.py
@@ -1,4 +1,11 @@
 import psutil
 
-print(psutil.cpu_percent())
-print(psutil.virtual_memory())
+# CPU usage percentage
+print(psutil.cpu_percent(interval=1))
+
+# CPU usage per core
+print(psutil.cpu_percent(interval=1, percpu=True))
+
+# Number of physical cores and logical CPUs
+print(psutil.cpu_count(logical=False))  # Physical cores
+print(psutil.cpu_count(logical=True))   # Logical CPUs (includes hyperthreading)
