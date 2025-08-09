116c54cb53a3828789eb5ee5becf136f5dc5992b
Dycouzt <dieacost@outlook.com>
2025-08-08 22:51:48 -0600

    psutil module updated

--- SystemAutomation/psutil_module.py
+++ SystemAutomation/psutil_module.py
@@ -9,3 +9,14 @@ print(psutil.cpu_percent(interval=1, percpu=True))
 # Number of physical cores and logical CPUs
 print(psutil.cpu_count(logical=False))  # Physical cores
 print(psutil.cpu_count(logical=True))   # Logical CPUs (includes hyperthreading)
+
+# Virtual memory (RAM)
+mem = psutil.virtual_memory()
+print(mem.total)      # Total RAM in bytes
+print(mem.available)  # Available RAM
+print(mem.used)       # Used RAM
+print(mem.percent)    # Usage percentage
+
+# Swap memory
+swap = psutil.swap_memory()
+print(swap.total, swap.used, swap.percent)
