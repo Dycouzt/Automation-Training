fca4cea4bd9b30563131801aaa4e30fcf192190f
Dycouzt <dieacost@outlook.com>
2025-08-23 21:30:13 -0600

    shutil module updated

--- SystemAutomation/shutil_module.py
+++ SystemAutomation/shutil_module.py
@@ -1,10 +1,1 @@
-import shutil
-
-# Copy a file (contents + permissions)
-shutil.copy("source.txt", "destination.txt")
-
-# Copy a file with metadata (permissions, timestamps)
-shutil.copy2("source.txt", "destination.txt")
-
-# Copy an entire directory
-shutil.copytree("source_folder", "destination_folder")
+# Topic Review shutil module.
