dc69877a70596f4903a72a4c468679a0852ac24b
Dycouzt <dieacost@outlook.com>
2025-08-23 21:30:42 -0600

    subprocess module updated

--- SystemAutomation/subprocess_module.py
+++ SystemAutomation/subprocess_module.py
@@ -1,11 +1,1 @@
-import subprocess
-
-# Run a simple shell command
-result = subprocess.run(['ls', '-la'], capture_output=True, text=True)
-
-# Output the result
-print(result.stdout)
-
-ping_result = subprocess.run(['ping', 'google.com'], capture_output=True, text=True)
-
-print(ping_result.stdout)
+# Topic Review subprocess module.
