9aa731bbe49795b97b1525c4d5091c5bc4e4116f
Dycouzt <dieacost@outlook.com>
2025-08-26 23:07:30 -0600

    os module update

--- SystemAutomation/os_module.py
+++ SystemAutomation/os_module.py
@@ -10,9 +10,12 @@ print(f"after: {os.getcwd()}")
 
 print(os.listdir(".")) # Lists all files in the current directory. os.listdir(path=".")
 
-os.mkdir("new_folder") # Creates a single new directory in the current path.
-os.makedirs("new_folder/nested_folder", exist_ok=True) # This won't raise error if the folder already exists.
-print(os.listdir("new_folder")) # Expected output: "nested_folder"
+os.mkdir("new_directory") # Creates a single new directory in the current path.
+os.makedirs("new_directory_2.0/nested_folder", exist_ok=True) # This won't raise error if the folder already exists.
+print(os.listdir("new_directory_2.0")) # Expected output: "nested_folder"
 
 os.remove("image.txt") # Deletes file "image.txt"
+os.rmdir("new_directory") # Deletes a single directory.
+os.removedirs("new_directory_2.0/nested_folder") # Removes folder and then "nested" if empty.
+
 
