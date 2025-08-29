28b91ef7a536c70bcaf4792375ecd946ee5753f7
Dycouzt <dieacost@outlook.com>
2025-08-28 23:36:46 -0600

    pathlib module updated

--- SystemAutomation/pathlib_module.py
+++ SystemAutomation/pathlib_module.py
@@ -1,1 +1,5 @@
-# Topic Review pathlib module.
+# Topic Review pathlib module. Used for object-oriented filesystem paths. Replaces some OS and OS.path functions.
+
+from pathlib import Path
+
+print(Path.cwd) # Prints current working directory.
