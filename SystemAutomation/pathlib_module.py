a9a5d38d83b56a53c0f935b34092b0fe1ab20159
Dycouzt <dieacost@outlook.com>
2025-08-28 23:43:58 -0600

    pathlib module updated

--- SystemAutomation/pathlib_module.py
+++ SystemAutomation/pathlib_module.py
@@ -3,6 +3,15 @@
 from pathlib import Path
 
 print(Path.cwd) # Prints current working directory.
+
 p = Path("folder/subfolder/image.txt")
 print(p) # Creates a path object.
+
+print(Path.home) # Returns the user’s home directory.
+
+print(p.Path.exists) # Checks if a path exists.
+
+p2 = Path("file.txt")
+print(p.is_file()) # Distinguishes between files and directories.
+print(p.is_dir())
 
