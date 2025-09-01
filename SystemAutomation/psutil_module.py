2d457eea093b559adf63e3cdf5edce5262e7eaad
Dycouzt <dieacost@outlook.com>
2025-08-31 23:37:42 -0600

    psutil module updated

--- SystemAutomation/psutil_module.py
+++ SystemAutomation/psutil_module.py
@@ -1,1 +1,5 @@
-# Topic Review
+"""
+ Topic Review psutil module. it is a cross-platform library for retrieving information about:
+System resources (CPU, memory, disks, network).
+Running processes (start time, PID, memory/cpu usage, etc.).
+ """
