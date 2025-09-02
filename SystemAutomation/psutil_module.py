d6fed6baf363f77b78c3b1fab86f745968069c94
Dycouzt <dieacost@outlook.com>
2025-09-01 21:34:54 -0600

    psutil module updated

--- SystemAutomation/psutil_module.py
+++ SystemAutomation/psutil_module.py
@@ -6,7 +6,16 @@ Running processes (start time, PID, memory/cpu usage, etc.).
 
 import psutil
 
+# CPU Information
+
 print(psutil.cpu_count(logical=True))   # counts hyper-threaded cores. e.g., 8
 print(psutil.cpu_count(logical=False))  # physical cores only. e.g., 4
 
 print(psutil.cpu_percent(interval=1))  # e.g., 12.5 (interval=1) -> waits 1 second before measuring.
+
+print(psutil.cpu_times()) # Returns time spent by CPU in different modes (user, system, idle).
+ # Example output: scputimes(user=1234.56, system=789.01, idle=45678.9, ...)
+
+# Memory Information
+
+
