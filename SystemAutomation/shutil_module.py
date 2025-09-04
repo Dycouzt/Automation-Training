c0647b18c9fb52453b437f79b331c2223aaf1842
Dycouzt <dieacost@outlook.com>
2025-09-04 23:58:26 +0200

    shutil module updated

--- SystemAutomation/shutil_module.py
+++ SystemAutomation/shutil_module.py
@@ -7,3 +7,9 @@ shutil.copy("data.csv", "backup_data.csv")
 Copies a file from src to dst. Preserves content but not metadata (permissions, timestamps).
 In this case, "data.csv" is copied as "backup_data.csv" in the same directory.
 """
+shutil.copy("data.csv", "backup_data_metadata.csv") # Copies a file and preserves metadata.
+
+shutil.copytree("dataset", "dataset_backup", dirs_exist_ok=True)
+"""
+
+"""
