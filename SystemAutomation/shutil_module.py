660be90b96cc4c2b423d09a28def1c188f3a5837
Dycouzt <dieacost@outlook.com>
2025-09-04 23:56:37 +0200

    shutil module updated

--- SystemAutomation/shutil_module.py
+++ SystemAutomation/shutil_module.py
@@ -1,1 +1,9 @@
-# Topic Review shutil module.
+# Topic Review shutil module. shutil stands for shell utilities.
+
+import shutil
+
+shutil.copy("data.csv", "backup_data.csv")
+"""
+Copies a file from src to dst. Preserves content but not metadata (permissions, timestamps).
+In this case, "data.csv" is copied as "backup_data.csv" in the same directory.
+"""
