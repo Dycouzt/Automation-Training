c22d6f72114ccabc3fdc47242dff9e4c909fe841
Dycouzt <dieacost@outlook.com>
2025-09-05 19:57:50 +0200

    subprocess module updated

--- SystemAutomation/subprocess_module.py
+++ SystemAutomation/subprocess_module.py
@@ -1,1 +1,8 @@
 # Topic Review subprocess module.
+
+import subprocess
+
+subprocess.run() # Simplest way to run a command. Waits until command finishes and returns a CompletedProcess object.
+
+result = subprocess.run(["echo", "Hello"], capture_output=True, text=True) # example.
+print(result.stdout) # expected output: "Hello".
