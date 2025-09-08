bb5829845f6ac0729d7fdf66fcc013c0557b59bc
Dycouzt <dieacost@outlook.com>
2025-09-08 15:24:17 +0200

    os_exercises module updated

--- SystemAutomation/AutomationExercises/os_practice.py
+++ SystemAutomation/AutomationExercises/os_practice.py
@@ -2,25 +2,37 @@
 
 import os
 
+"""
+1. Create a script that:
+    - Creates a new directory named practice.
+    - Changes into that directory.
+    - Creates three empty files: a.txt, b.txt, c.txt.
+    - Lists all files in the directory.
+"""
+
 print(os.getcwd())
 
 def practice_directory_creation():
+    
+    # Create directory (ignore if it already exists)
     os.makedirs("practice", exist_ok=True) # exist_ok=True only usable for os.mkdirs! 
     print("success! ")
-
     expected_cwd = os.path.join(os.getcwd(), "practice")
     cwd = os.getcwd()
-    print(cwd)
+    print("Current working directory:", cwd)
 
+    # Change into it
     if cwd == expected_cwd:
         os.chdir("practice")
     else:
          print("practice directory doesn't exist yet! ")
 
+    # Create three empty files
     for filename in ["a.txt", "b.txt", "c.txt"]:
             open(filename, "w").close()     # "W" means write. Important to .close() after creating files.
     print("practice directory not exists yet! ")
     
+     # List files
     print(os.listdir("."))
 
 practice_directory_creation()
