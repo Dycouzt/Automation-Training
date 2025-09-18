0271dd30adce962f0578cf152cdac7a008a0e539
Dycouzt <dieacost@outlook.com>
2025-09-18 11:38:07 +0200

    sys practice module updated

---
+++ SystemAutomation/AutomationExercises/sys_practice.py
@@ -0,0 +1,18 @@
+"""
+1. Argument Parser: Write a script using sys.argv that accepts a filename 
+and prints whether it exists. Exit with 0 if found, 1 otherwise.
+"""
+
+"""
+2. Cross-Platform Script: Use sys.platform to print OS-specific commands 
+(e.g., ls for Linux, dir for Windows).
+"""
+
+""" 3. Version Checker: Create a script that exits with an error if Python version < 3.10."""
+
+"""Custom Logger: Redirect sys.stdout to a file and log messages there."""
+
+"""
+4. Memory Tracker: Write a script that calculates 
+and prints the memory size of a list of integers from 1-1000.
+"""
