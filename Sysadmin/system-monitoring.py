eea18ba8d4b7130ea058fd03516d4ab7911b1da3
Dycouzt <dieacost@outlook.com>
2025-10-01 12:18:43 +0200

    system monitoring script updated

--- Sysadmin/system-monitoring.py
+++ Sysadmin/system-monitoring.py
@@ -11,9 +11,32 @@ import sys
 CPU_THRESHOLD = 75
 MEM_THRESHOLD = 70
 
-try:
-    cpu = psutil.cpu_count(interval=1)
-    time.sleep(1)
+threshold_alert = []
 
-except (psutil.NoSuchProcess, psutil.AccessDenied, PermissionError):
-    continue
+# Initialize CPU measurement
+cpu_usage = psutil.cpu_percent(interval=1)
+mem_usage = psutil.virtual_memory()
+
+ten_percent = []
+
+    # Initialize CPU measurement
+for process in psutil.process_iter():
+    try:
+        process.cpu_percent(interval=None)
+    except (psutil.NoSuchProcess, psutil.AccessDenied, PermissionError):
+        continue
+
+time.sleep(1)
+
+for process in psutil.process_iter(['pid', 'name']):
+    try:
+        cpu_usage = process.cpu_percent(interval=None)
+        if cpu_usage > 10:
+            ten_percent.append({
+                "pid": process.pid,
+                "name": process.name(),
+                "cpu_percent": cpu_usage
+            })
+    except (psutil.NoSuchProcess, psutil.AccessDenied, PermissionError):
+        continue
+    
