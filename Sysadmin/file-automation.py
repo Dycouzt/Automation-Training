8ff2687e5e2f6651cc7163c22be81e010e478976
Dycouzt <dieacost@outlook.com>
2025-10-08 12:51:23 +0200

    file_automation.py updated

--- Sysadmin/file-automation.py
+++ Sysadmin/file-automation.py
@@ -4,23 +4,46 @@ Monitor a directory for changes (creation/modification/deletion).
 Automate backups: Copy files from one directory to another with timestamped folders.
 Parse log files: Count error/warning lines from system/application logs.
 """
-
+import time, datetime, shutil, re
 from pathlib import Path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-import time
+from collections import defaultdict
 
 base_dir = Path(__file__).resolve().parent
 
-def filter_extension():
-    files = [item for item in base_dir.iterdir() if item.is_file()]
+# List files recursively and filter by ".log" extension
+def filter_extension(match):
+    files = [item for item in base_dir.rglob(match)]
 
-    for files in files:
-        file_ext = r.compile(r'/./w+')
-        print(file_ext)
+    for file in files:
+        print(file)
 
-filter_extension()
+# Automate backup of files filtered by given extension. 
+def get_backup(intended_backup):
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    backup_dir = base_dir / f"files_backup_{timestamp}"
 
+    if intended_backup.exists():
+        shutil.copytree(intended_backup, backup_dir)
+        print(f"{intended_backup} backup created to {backup_dir} -> {timestamp}")
+    else:
+        print(f"Files for backup not found. ")
+
+# Parse given logs 
+def log_parse(log_files):
+    pattern = re.compile(r"/bERROR/b")
+    error_counts = defaultdict(int)
+    for file in log_files:
+        with file.open("r") as f:
+            for line in f:
+                if pattern.search(line):
+                    date = line.split()[0]
+                    error_counts[date] += 1
+    return error_counts
+                
+
+# Monitor directory for changes.
 class Watcher:
     def __init__(self, directory_to_watch):
         self.DIRECTORY_TO_WATCH = directory_to_watch
@@ -37,7 +60,6 @@ class Watcher:
             self.observer.stop()
         self.observer.join()
 
-
 class Handler(FileSystemEventHandler):
     def on_modified(self, event):
         print(f"Modified: {event.src_path}")
@@ -50,5 +72,6 @@ class Handler(FileSystemEventHandler):
 
 
 if __name__ == '__main__':
+    intended_backup = filter_extension("**.log")
     path = "/path/to/watch"
     Watcher(path).run()
